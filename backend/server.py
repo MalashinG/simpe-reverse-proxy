54944dbee08c574f54e14deef1a27e5d414388a9
MalashinG <118243807+MalashinG@users.noreply.github.com>
2026-03-23 11:26:25 +0300

    Change greeting message in server response

--- backend/server.py
+++ backend/server.py
@@ -6,6 +6,6 @@ class Handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'Hello from Effective Mobile!\n')
+            self.wfile.write(b'Hello!\n')
 
 HTTPServer(('0.0.0.0', 8080), Handler).serve_forever()
